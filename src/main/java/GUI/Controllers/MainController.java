3b75eb7db6ae4e4dfd191aed6d43a554c2caab96
Nguyen Khai Hoan <20020666@vnu.edu.vn>
2022-07-19 21:49:55 +0700

    Add MainView and MainController,add persistence.xml, update pom.xml

---
+++ src/main/java/GUI/Controllers/MainController.java
@@ -0,0 +1,21 @@
+package GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class MainController {
+    @FXML
+    public StackPane viewStack;
+    @FXML
+    public Button searchVocabButton;
+    @FXML
+    public Button translateTextButton;
+
+    public void searchVocabButton_OnClicked(ActionEvent actionEvent) {
+    }
+
+    public void translateTextButton_OnClicked(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/GUI/GuiApp.java
+++ src/main/java/GUI/GuiApp.java
@@ -1,4 +1,22 @@
 package GUI;
 
-public class GuiApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GuiApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+        System.exit(0);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/MainView.fxml"));
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Từ điển Anh-Việt");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
