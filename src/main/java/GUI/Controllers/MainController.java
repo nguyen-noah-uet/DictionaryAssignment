505887e850a0331a1964c5bad9db94fa9411e2ba
Nguyen Khai Hoan <20020666@vnu.edu.vn>
2022-07-25 15:30:56 +0700

    Thêm tính năng navigation Thêm Icon Thêm View để tra từ

--- src/main/java/GUI/Controllers/MainController.java
+++ src/main/java/GUI/Controllers/MainController.java
@@ -2,10 +2,18 @@ package GUI.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
-public class MainController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
     @FXML
     public StackPane viewStack;
     @FXML
@@ -13,9 +21,32 @@ public class MainController {
     @FXML
     public Button translateTextButton;
 
-    public void searchVocabButton_OnClicked(ActionEvent actionEvent) {
+    public void searchVocabButton_OnClicked(ActionEvent actionEvent) throws IOException {
+        searchVocabButton.setStyle("-fx-background-color: #3c3f41; ");
+        translateTextButton.setStyle("-fx-background-color: #4b4b4c; ");
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/SearchVocabView.fxml"));
+        viewStack.getChildren().removeAll();
+        viewStack.getChildren().setAll(root);
     }
 
-    public void translateTextButton_OnClicked(ActionEvent actionEvent) {
+    public void translateTextButton_OnClicked(ActionEvent actionEvent) throws IOException {
+        searchVocabButton.setStyle("-fx-background-color:  #4b4b4c; ");
+        translateTextButton.setStyle("-fx-background-color: #3c3f41; ");
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/TranslateTextView.fxml"));
+        viewStack.getChildren().removeAll();
+        viewStack.getChildren().setAll(root);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            searchVocabButton.setStyle("-fx-background-color: #3c3f41; ");
+            translateTextButton.setStyle("-fx-background-color: #4b4b4c; ");
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Views/SearchVocabView.fxml")));
+            viewStack.getChildren().removeAll();
+            viewStack.getChildren().setAll(root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/GUI/GuiApp.java
+++ src/main/java/GUI/GuiApp.java
@@ -1,12 +1,15 @@
 package GUI;
 
+import GUI.Controllers.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GuiApp extends Application {
+    private MainController mainController;
     public static void main(String[] args) {
         launch(args);
         System.exit(0);
@@ -15,8 +18,12 @@ public class GuiApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Views/MainView.fxml"));
         Parent root = fxmlLoader.load();
+        mainController = fxmlLoader.getController();
+        primaryStage.getIcons().add(new Image("Image/EnglishIcon.png"));
         primaryStage.setTitle("Từ điển Anh-Việt");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinWidth(850);
+        primaryStage.setMinHeight(650);
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 }
