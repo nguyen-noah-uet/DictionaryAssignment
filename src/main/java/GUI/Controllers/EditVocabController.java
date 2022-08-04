c45d8b0b886331d922ce2c81672973a4ebb46641
Nguyen Khai Hoan <20020666@vnu.edu.vn>
2022-08-04 22:51:50 +0700

    Add update feature, update UI

--- src/main/java/GUI/Controllers/EditVocabController.java
+++ src/main/java/GUI/Controllers/EditVocabController.java
@@ -8,6 +8,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,6 +19,8 @@ import java.util.ResourceBundle;
 public class EditVocabController implements Initializable {
     private Vocabulary vocabulary;
     private IDataService<Vocabulary> dataService;
+    @FXML
+    public WebView webView;
     @FXML
     public TextField wordTextField;
     @FXML
@@ -27,11 +32,17 @@ public class EditVocabController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        webView.setFontScale(1.2);
+        meaningTextArea.textProperty().addListener((observable, oldValue, newValue) -> {
+            webView.getEngine().loadContent(meaningTextArea.getText());
+        });
     }
 
     public void saveButton_OnClicked(ActionEvent actionEvent) throws IOException {
-        MainController.getMainController().navigateToTranslateTextView();
+        vocabulary.setWord(wordTextField.getText());
+        vocabulary.setDetailString(meaningTextArea.getText().replace("\n", ""));
+        dataService.update(vocabulary.getId(), vocabulary);
+        MainController.getMainController().navigateToSearchVocabView();
     }
 
     public void goBackButton_OnClicked(ActionEvent actionEvent) throws IOException {
@@ -40,6 +51,14 @@ public class EditVocabController implements Initializable {
 
     public void setVocabulary(Vocabulary vocabulary) {
         this.vocabulary = vocabulary;
+        wordTextField.setText(vocabulary.getWord());
+        meaningTextArea.setText(reformatHtml(vocabulary.getDetailString()));
+        webView.getEngine().loadContent(vocabulary.getDetailString());
+    }
+
+    private String reformatHtml(String detailString) {
+        Document doc = Jsoup.parseBodyFragment(vocabulary.getDetailString());
+        return doc.body().children().toString();
     }
 
     public void setDataService(IDataService<Vocabulary> dataService) {
