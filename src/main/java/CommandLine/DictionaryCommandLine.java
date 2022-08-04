5b12f011fa9f4e6513c7cc1f66a00d98ad2144bd
dohieu <56931122+domindo@users.noreply.github.com>
2022-08-04 10:03:51 +0700

    Add files via upload
    Cải tiến lần 1 thêm  thêm dictionaryAdvanced

--- src/main/java/CommandLine/DictionaryCommandLine.java
+++ src/main/java/CommandLine/DictionaryCommandLine.java
@@ -1,27 +1,31 @@
-package CommandLine;
-
 public class DictionaryCommandLine {
-    DictionaryManagement dictionaryManagement = new DictionaryManagement();
+  DictionaryManagement dictionaryManagement = new DictionaryManagement();
+
+  public void showAllWords() {
+    System.out.println("No\t\t  |English\t\t\t\t  |VietNamese");
+    for (int i = 0; i < Dictionary.words.size(); i++) {
+      System.out.printf(
+          "%-10s%-25s%-25s\n",
+          i + 1,
+          Dictionary.words.get(i).getWord_target(),
+          Dictionary.words.get(i).getWord_explain());
+    }
+  }
+
+  public void dictionaryBasic(Dictionary dictionary) {
+    dictionaryManagement.insertFromCommandline(dictionary);
+    showAllWords();
+  }
+
+  public void dictionaryAdvanced(Dictionary dictionary) {
+    dictionaryManagement.insertFromFile(dictionary);
+    showAllWords();
+    dictionaryManagement.dictionaryLookup(dictionary);
+  }
+
+  public static void main(String[] args) throws Exception {
+    DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
     Dictionary dictionary = new Dictionary();
-    public void showAllWords(){
-        System.out.println("No\t|English\t\t|VietNamese");
-        for (int i = 0; i < Dictionary.words.size(); i++) {
-            System.out.printf("%-10s%-30s%-30s\n", i + 1, Dictionary.words.get(i).getWord_target(), Dictionary.words.get(i).getWord_explain());
-        }
-    }
-    public void dictionaryBasic() {
-        dictionaryManagement.insertFromCommandline();
-        showAllWords();
-    }
-
-
-//
-//    public static void main (String[] args){
-//
-//        DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
-//
-//        dictionaryCommandLine.dictionaryBasic();
-//
-//    }
+    dictionaryCommandLine.dictionaryAdvanced(dictionary);
+  }
 }
-
