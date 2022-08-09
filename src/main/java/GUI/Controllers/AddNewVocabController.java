08592e1c4b246cd609a5259287a4b480441eb8a1
JamesHoan1999 <tienhoan2405@gmail.com>
2022-08-09 09:57:15 +0700

    Thêm AddNewVocabView

--- src/main/java/GUI/Controllers/AddNewVocabController.java
+++ src/main/java/GUI/Controllers/AddNewVocabController.java
@@ -2,13 +2,45 @@ package GUI.Controllers;
 
 import GUI.Entities.Vocabulary;
 import GUI.Services.Data.IDataService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AddNewVocabController implements Initializable {
+    @FXML
+    public TextField newVocabTextField;
+    @FXML
+    public TextField phoneticTextField;
+    @FXML
+    public ComboBox<String> wordTypeComboBox1;
+    @FXML
+    public TextArea wordMeaning1;
+    @FXML
+    public ComboBox<String> wordTypeComboBox2;
+    @FXML
+    public ComboBox<String> wordTypeComboBox3;
+    @FXML
+    public TextArea wordMeaning3;
+    @FXML
+    public TextArea wordMeaning2;
+    @FXML
+    public WebView webView;
+    @FXML
+    public Button submitButton;
+    @FXML
+    public Button goBackButton;
     private IDataService<Vocabulary> dataService;
+    private final ArrayList<String> wordTypes = new ArrayList<>();
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -16,5 +48,13 @@ public class AddNewVocabController implements Initializable {
 
     public void setDataService(IDataService<Vocabulary> dataService) {
         this.dataService = dataService;
+    }
+
+    public void submitButton_OnClicked(ActionEvent actionEvent) throws IOException {
+        MainController.getMainController().navigateToSearchVocabView();
+    }
+
+    public void goBackButton_OnClicked(ActionEvent actionEvent) throws IOException {
+        MainController.getMainController().navigateToSearchVocabView();
     }
 }
